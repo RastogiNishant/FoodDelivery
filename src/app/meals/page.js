c537faeb48fcdb9cb2ed7ff52af3c211a46302f5
NV Rastogi <rastoginishant@yahoo.in>
2024-08-31 16:52:30 +0530

    Add Meals Page

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,5 +1,25 @@
-const AboutPage = () => {
-	return <main>About</main>;
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+const MealsPage = () => {
+	return (
+		<header className={classes.header}>
+			<h1>
+				Delicious meals, created{" "}
+				<span className={classes.highlight}>by you</span>
+			</h1>
+			<p>
+				Choose your favorite recipe and cook it yourself. It is easy and
+				fun!
+			</p>
+			<p className={classes.cta}>
+				<Link href='/meals/share'>Share Your Favorite Recipe</Link>
+			</p>
+			<main className={classes.main}>
+				<MealsGrid meals={[]} />
+			</main>
+		</header>
+	);
 };
 
-export default AboutPage;
+export default MealsPage;
