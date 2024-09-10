1c6366efda4e46903810cf105a05baf1140ea667
NV Rastogi <rastoginishant@yahoo.in>
2024-09-10 21:29:58 +0530

    Added Server Action

---
+++ src/lib/actions.js
@@ -0,0 +1,13 @@
+"use server";
+
+export const shareMeal = (formData) => {
+	const meal = {
+		title: formData.get("title"),
+		summary: formData.get("summary"),
+		instruction: formData.get("instruction"),
+		image: formData.get("image"),
+		creator: formData.get("creator"),
+		creator_email: formData.get("creator_email"),
+	};
+	console.log(meal);
+};
