c91d5822c932bbfc04e775323c98c67bbe62c444
NV Rastogi <rastoginishant@yahoo.in>
2024-09-10 22:45:48 +0530

    Added Save Meal Func

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,13 +1,17 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+
 export const shareMeal = async (formData) => {
 	const meal = {
 		title: formData.get("title"),
 		summary: formData.get("summary"),
 		instruction: formData.get("instruction"),
 		image: formData.get("image"),
-		creator: formData.get("creator"),
-		creator_email: formData.get("creator_email"),
+		creator: formData.get("name"),
+		creator_email: formData.get("email"),
 	};
-	console.log(meal);
+	await saveMeal(meal);
+	redirect("/meals");
 };
