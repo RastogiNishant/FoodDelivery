14733253aff8d3663e6614c675648733a529be61
NV Rastogi <rastoginishant@yahoo.in>
2024-09-13 23:15:58 +0530

    Added S3 Bucket For Images

--- src/app/meals/[mealSlug]/page.js
+++ src/app/meals/[mealSlug]/page.js
@@ -4,22 +4,26 @@ import { getMeal } from "@/lib/meals";
 
 const MealDetailsPage = ({ params }) => {
 	const meal = getMeal(params.mealSlug);
-	const instructions = meal?.instructions.replace(/\n/g, "<br/>");
+	const instructions = meal?.instructions?.replace(/\n/g, "<br/>") || "";
 	return (
 		<>
 			<header className={classes.header}>
 				<div className={classes.image}>
-					<Image src={meal.image} alt={meal.title} fill />
+					<Image
+						src={`https://nextjs-s3-bucket-13.s3.eu-central-1.amazonaws.com/${meal?.image}`}
+						alt={meal?.title}
+						fill
+					/>
 				</div>
 				<div className={classes.headerText}>
-					<h1>{meal.title}</h1>
+					<h1>{meal?.title}</h1>
 					<p className={classes.creator}>
 						by{" "}
-						<a href={`mailto:${meal.creator_email}`}>
-							{meal.creator}
+						<a href={`mailto:${meal?.creator_email}`}>
+							{meal?.creator}
 						</a>
 					</p>
-					<p className={classes.summary}>{meal.summary}</p>
+					<p className={classes.summary}>{meal?.summary}</p>
 				</div>
 			</header>
 			<main>
