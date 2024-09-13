5fb779fc8c0bcc20cf65249eafd38b9987085783
NV Rastogi <rastoginishant@yahoo.in>
2024-09-13 23:34:26 +0530

    Add Upload S3 Functionality

--- src/lib/meals.js
+++ src/lib/meals.js
@@ -1,8 +1,11 @@
-import fs from "node:fs";
+import { S3 } from "@aws-sdk/client-s3";
 import sql from "better-sqlite3";
 import slugify from "slugify";
 import xss from "xss";
 
+const s3 = new S3({
+	region: "us-east-1",
+});
 const db = sql("meals.db");
 
 export const getMeals = async () => {
@@ -18,20 +21,32 @@ export const saveMeal = async (meal) => {
 	meal.instructions = xss(meal.instructions);
 
 	const extension = meal.image.name.split(".").pop();
-	const filename = `${meal.slug}.${extension}`;
+	const fileName = `${meal.slug}.${extension}`;
 
-	const stream = fs.createWriteStream(`public/images/${filename}`);
 	const bufferedImage = await meal.image.arrayBuffer();
 
-	stream.write(Buffer.from(bufferedImage), (error) => {
-		if (error) {
-			throw new Error("Saving image failed!");
-		}
+	s3.putObject({
+		Bucket: "nextjs-s3-bucket-13",
+		Key: fileName,
+		Body: Buffer.from(bufferedImage),
+		ContentType: meal.image.type,
 	});
 
-	meal.image = `/images/${filename}`;
+	meal.image = fileName;
 
 	db.prepare(
-		`INSERT INTO meals (title, summary, instructions, creator, creator_email, image, slug) VALUES (@title, @summary, @instructions, @creator, @creator_email, @image, @slug)`,
+		`
+	  INSERT INTO meals
+		(title, summary, instructions, creator, creator_email, image, slug)
+	  VALUES (
+		@title,
+		@summary,
+		@instructions,
+		@creator,
+		@creator_email,
+		@image,
+		@slug
+	  )
+	`,
 	).run(meal);
 };
